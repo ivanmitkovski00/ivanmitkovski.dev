b163c2d9cd0c59a1a198f65a70e1183e6f27ae6b
ivanmitkovski00 <ivanmitkovski2@gmail.com>
2025-04-18 23:18:39 +0200

    Started on about section, and fixed some code in the hero section

---
+++ src/components/About.tsx
@@ -0,0 +1,16 @@
+export default function About() {
+  return (
+    <div className="px-7 mt-18">
+      <h3 className="text-white text-7xl font-semibold">About</h3>
+      <div className="w-1/2 mt-18">
+        <p className="text-white text-lg md:text-xl text-justify font-thin leading-relaxed tracking-wide">
+          Thoughtful software engineer focused on clean, user-centered
+          solutions. I value clarity, collaboration, and steady growth. Building
+          practical, well-designed experiences with care, curiosity, and a
+          strong attention to detail.
+          <div></div>
+        </p>
+      </div>
+    </div>
+  );
+}
